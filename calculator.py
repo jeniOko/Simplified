9174b72bea6fc5f4b94735eeb4d26b45fc570edc
jeniOko <103377324+jeniOko@users.noreply.github.com>
2024-04-17 23:57:00 +0300

    Create calculator.py
    a simple calculator that adds, subtracts, multiplies, divides,  shows the remainder after division, shows the integer value after division and calculate the power of two numbers

---
+++ calculator.py
@@ -0,0 +1,18 @@
+first_number = float(input("Enter a number "))
+second_number = float(input("Enter another number "))
+
+Sum = (first_number) + (second_number)
+Difference = (first_number) - (second_number)
+Multiplication = (first_number) * (second_number)
+Exponent = (first_number) ** (second_number) # power of
+Division = (first_number) / (second_number)
+DoubleDivide = (first_number) // (second_number) # returns an integer of division
+Modulo = (first_number) % (second_number) #remainder
+
+print("The sum is " +str(Sum))
+print("The difference is " + str(Difference))
+print("The multiplication value is " +str(Multiplication))
+print("The exponent value is " +str(Exponent))
+print("The division value is " +str(Division))
+print("The integer value after division is " +str(DoubleDivide))
+print("The remainder is " +str(Modulo))
